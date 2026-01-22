3f50c92fdc363d0cc865c4af4aba67e74fb8d1ae
Melissa Gentles <melissa.gentles@engineering.digital.dwp.gov.uk>
2026-01-22 14:17:16 +0000

    routing fixes, combined contact and address, and dynamic answers page creation
    GitOrigin-RevId: 5389c20799e523a08ca0de8cc94014a059f60f99

--- app/routes.js
+++ app/routes.js
@@ -6,18 +6,7 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-
-
 //Routes go here
-router.post('/funding-scenario', function(request, response) {
- 
-  var fundingScenario = request.session.data['funding-scenario'];
-  if (fundingScenario === "Health authority" || fundingScenario === "Claimant") {
-      response.redirect("payability/v1/stay/check-answers")
-  } else {
-    response.redirect("payability/v1/stay/funding-address.html")
-}
-})
 
 
 
--- app/routes/payability-routes-v1.js
+++ app/routes/payability-routes-v1.js
@@ -3,6 +3,16 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 //Routes go here
+router.post('/funding-scenario', function(request, response) {
+ 
+  var fundingScenario = request.session.data['funding-scenario'];
+  if (fundingScenario === "Health authority" || fundingScenario === "Claimant") {
+      response.redirect("payability/v1/stay/check-answers")
+  } else {
+    response.redirect("payability/v1/stay/funding-address.html")
+}
+})
 
-//xxport routes
+
+//export routes
 module.exports = router;
