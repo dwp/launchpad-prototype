e53897679cd314e738e68d8fc5328b5afdc00909
Melissa Gentles <melissa.gentles@engineering.digital.dwp.gov.uk>
2026-01-23 11:51:41 +0000

    Updates from inital share with aidan
    GitOrigin-RevId: af2d710a9bf9ab5d321b659918103dc159567895

--- app/routes/payability-routes-v1.js
+++ app/routes/payability-routes-v1.js
@@ -3,13 +3,14 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 //Routes go here
+
 router.post('/funding-scenario', function(request, response) {
  
   var fundingScenario = request.session.data['funding-scenario'];
-  if (fundingScenario === "Health authority" || fundingScenario === "Claimant") {
+  if (fundingScenario === "Health authority" || fundingScenario === "Claimant" || fundingScenario === "Not known") {
       response.redirect("payability/v1/stay/check-answers2")
   } else {
-    response.redirect("payability/v1/stay/funding-address.html")
+    response.redirect("payability/v1/stay/funding-address")
 }
 })
 
@@ -22,7 +23,6 @@ router.post('/add-another-stay', function(request, response) {
     response.redirect("payability/v1/stay/review-payment")
 }
 })
-
 
 
 //export routes
