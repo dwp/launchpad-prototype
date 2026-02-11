6035ca7c328f2dc57f931ba1b85bcab7c5c261de
Melissa Gentles <melissa.gentles@engineering.digital.dwp.gov.uk>
2026-02-11 11:18:01 +0000

    pushing all changes
    GitOrigin-RevId: 2bb99ff69601da852ba0f93d3840dbb89959595a

--- app/routes/payability-routes-v1.js
+++ app/routes/payability-routes-v1.js
@@ -4,15 +4,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 //Routes go here
 
-router.post('/funding-scenario', function(request, response) {
- 
-  var fundingScenario = request.session.data['funding-scenario'];
-  if (fundingScenario === "Health authority" || fundingScenario === "Claimant" || fundingScenario === "Not known") {
-      response.redirect("payability/v1/stay/check-answers2")
-  } else {
-    response.redirect("payability/v1/stay/funding-address")
-}
-})
+
 
 router.post('/add-another-stay', function(request, response) {
  
@@ -23,6 +15,39 @@ router.post('/add-another-stay', function(request, response) {
     response.redirect("payability/v1/stay/review-payment")
 }
 })
+
+
+router.post('/funding-scenario', function(request, response) {
+ var fundingSkip = request.session.data['funding-scenario'];
+  if (fundingSkip === "Health authority" || fundingSkip === "Claimant" ) {
+      response.redirect("payability/v1/stay/check-answers2")
+  } else {
+    response.redirect("payability/v1/stay/funding-address")
+}
+})
+
+
+
+router.post('/accommodation-details', function (req, res) {
+  const data = req.body;
+  const errors = {};
+
+  if (!data['accomodation-name']) {
+    errors['accomodation-name'] = { text: "Enter the accomodation name" };
+  }
+
+  if (!data['address-town']) {
+    errors['address-town'] = { text: "Enter the town or city" };
+  }
+
+  if (Object.keys(errors).length) {
+    return res.render('payability/v1/stay/accomodation-details.html', { data, errors });
+  }
+
+  res.redirect('payability/v1/stay/funding-type');
+});
+
+
 
 
 //export routes
